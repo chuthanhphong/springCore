763a574db5ef93dec14a2aec45a52e7dc9fbdb50
ctphong <ctphong@cmcglobal.vn>
2022-10-14 11:26:35 +0700

    project demo

--- src/main/java/com/example/demo/demoDI/service/CustomerService.java
+++ src/main/java/com/example/demo/demoDI/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.example.demo.demoDI.service;
 
 import com.example.demo.demoDI.model.Customer;
 import com.example.demo.demoDI.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public class CustomerService implements IGenaralService<Customer> {
 
     //Field injection
-//    @Autowired
+    @Autowired
     private CustomerRepository customerRepository;
 
 //    public CustomerService(CustomerRepository customerRepository) {
